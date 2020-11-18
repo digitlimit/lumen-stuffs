7e0d21822fb210059eeca11fbcef9ff9b911f65c
Digitlimit Inc <frankemeks77@yahoo.com>
2020-11-18 20:24:16 +0100

    chore: add comment to user controller action

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,11 @@ use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
-
+    /**
+     * Return a collection of users
+     *
+     * @return UserResource
+     */
     public function index()
     {
         return UserResource::collection(User::all());
